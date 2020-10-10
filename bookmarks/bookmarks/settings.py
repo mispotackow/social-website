f24ea14f180827240d91e10d0e0e765e081bb59d
mispotackow <potackiw@gmail.com>
2020-10-10 11:45:31 +0300

    Just a test

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xxx'
+SECRET_KEY = 'just_a_test_from_another_computer'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
